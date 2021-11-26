559c68b3d64797339f9c4d060cdbed8d1af77734
Philippe Laval <philippe.laval@metaline.fr>
2021-11-26 20:58:54 +0100

    Add nugets and start of code

---
+++ TestPostgresql/BlogContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
+public class BlogContext : DbContext
+{
+    public DbSet<Blog> Blogs { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseNpgsql(@"Host=myserver;Username=mylogin;Password=mypass;Database=mydatabase");
+}
--- TestPostgresql/Program.cs
+++ TestPostgresql/Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Microsoft.EntityFrameworkCore;
+
 Console.WriteLine("Hello, World!");
+
+await using var ctx = new BlogContext();
+await ctx.Database.EnsureDeletedAsync();
+await ctx.Database.EnsureCreatedAsync();
+
+// Insert a Blog
+ctx.Blogs.Add(new Blog() { Name = "FooBlog" });
+await ctx.SaveChangesAsync();
+
+// Query all blogs who's name starts with F
+var fBlogs = await ctx.Blogs.Where(b => b.Name.StartsWith("F")).ToListAsync();
